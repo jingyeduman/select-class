203222957606958fd4dd9bc73881fda601958214
wuguanping <769933595@qq.com>
2022-07-17 15:01:45 +0800

    feat:test

---
+++ src/main/java/com/example/selectclass/SelectClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.selectclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/selectclass/SelectClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selectclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
